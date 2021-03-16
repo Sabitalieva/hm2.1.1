284ace3c780a5ecf023650ffeb4a68e36ae528d6
sabitalievanuskaiym <77196324+Sabitalieva@users.noreply.github.com>
2021-03-16 15:22:17 +0600

    Initial commit

---
+++ src/com/company/Apps.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.awt.*;
+
+public class Apps {
+
+        private Color color;
+
+        public Apps() {
+        }
+
+        public Color getColor() {
+            return color;
+        }
+
+        public Apps(Color color) {
+            this.color = color;
+        }
+        public String getInfo(){
+            return "Color: " + color ;
+        }
+    }
+
+
